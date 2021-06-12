adbb98ed0b30bf6bb340411c674b936be8230cb1
Anna J <64473440+Nidstaang@users.noreply.github.com>
2021-06-12 16:00:05 +0200

    .

---
+++ Game/test_Arcane-Quest.py
@@ -0,0 +1,8 @@
+### wrongwrong wrgon it's just a test
+from unittest import mock
+from unittest.mock import patch
+from Arcane_Quest import start, intro, bedchamber
+
+
+def test_start(self):
+    assert()
