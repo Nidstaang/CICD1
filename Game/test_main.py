54124d4ff27e4e5077948c30680035f51bf6f0df
Anna J <64473440+Nidstaang@users.noreply.github.com>
2021-06-12 14:44:12 +0200

    name change

--- Game/test_main.py
+++
@@ -1,9 +0,0 @@
-### wrongwrong wrgon it's just a test
-from unittest import mock
-from unittest.mock import patch
-from main import start, intro, bedchamber
-
-
-def test_start(self):
-    with mock.patch('sys.stdout', new=io.StringIO()) as fake_stdout:
-        start() 
