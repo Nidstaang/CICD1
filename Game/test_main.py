1e313c3541d81a0a5e2ff6164c5d544161edd13f
Anna J <64473440+Nidstaang@users.noreply.github.com>
2021-06-12 18:01:45 +0200

    .

--- Game/test_main.py
+++ Game/test_main.py
@@ -1,7 +1,7 @@
 ### wrongwrong wrgon it's just a test
 from unittest import mock
 from unittest.mock import patch
-from Arcane_Quest import start, intro, bedchamber
+from main import start, intro, bedchamber
 
 
 def test_start(self):
